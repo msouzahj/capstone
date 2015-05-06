f748d2ef6d814a9ec0ba4d4a0022ffa4457711e9
msouzahj <msouza7@mail.csuchico.edu>
2015-05-06 16:42:19 +0000

    last minute changes

--- captest/app/controllers/seasons_controller.rb
+++ captest/app/controllers/seasons_controller.rb
@@ -25,7 +25,9 @@ class SeasonsController < ApplicationController
 				@my_hash.store("#{event.name}",event.results)
 
 			   elsif @my_hash.has_key?("#{event.name}") == true	
-		 	    if (event.name == "High Jump" || event.name == "Long Jump" || event.name == "Triple Jump" || event.name =="Pole Vault" ||event.name == "Shot Put" || event.name == "Weight Throw" ||event.name == "Discus" || event.name =="Javelin")
+		 	    if (event.name == "High Jump" || event.name == "Long Jump" || event.name == "Triple Jump" || event.name =="Pole Vault" ||event.name == "Shot Put" || event.name == "Weight Throw" ||event.name == "Discus Throw" || event.name =="Javelin Throw" || event.name =="Hammer Throw")
+				if event.results != "NH"
+			
 			 	     @my_hash.keys.each do |key|
 				       if key.to_s == event.name.to_s
                                         if event.results.to_f > @my_hash[key].to_f
@@ -35,17 +37,21 @@ class SeasonsController < ApplicationController
 					end
 				       end	
 				      end
+				end
 			     elsif (event.name == "100m" || event.name == "200m" || event.name == "100mH" || event.name == "50m" || event.name == "55m" || event.name == "60m" || event.name == "50mH" || event.name == "55mH" || event.name =="60mH" || event.name == "400m" || event.name == "400mH" || event.name == "100mH")
+				if event.results != "DNF"
 				    @my_hash.keys.each do |key|
 			              if key.to_s == event.name.to_s
-                                        if  @my_hash[key] < event.results
+                                        if  @my_hash[key] > event.results
 						@my_hash[key] = event.results
-					elsif @my_hash[key] >= event.results
+					elsif @my_hash[key] <= event.results
 					   @my_hash[key] = @my_hash[key]
 					end
 				      end
 				    end
+				end
                              else
+				if event.results != "DNF"
 				    @my_hash.keys.each do |key|
 			              if key.to_s == event.name.to_s
                                         if  @my_hash[key] > event.results
@@ -55,6 +61,7 @@ class SeasonsController < ApplicationController
 					end
 				      end
 				    end
+				end
                             end
 		   	   end
 		end
